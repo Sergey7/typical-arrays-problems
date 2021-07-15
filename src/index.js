79b8f0f8ad66af1fe1739605703adfabc51b024b
Sergey7 <suvoo@mail.ru>
2021-07-15 16:22:56 +0300

    feat: implementation of functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+    let minimal = array[0];
+    
+    for (let i = 1 ; i < array.length; i++) {
+        if (minimal > array[i]) { 
+            minimal = array[i]
+        }
+    }
+    return minimal
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+    let maximum = array[0];
+    for (let i = 1 ; i < array.length; i++) {
+        if (maximum < array[i]) { maximum = array[i] }
+    }
+    return maximum
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+    let average = array[0];
+    for (let i = 1 ; i < array.length; i++) {
+        average += array[i];
+    }
+    return average / array.length
 }
+
